7792d1d540c59a0727842030c6f96d98f6d16a70
Ger Kelly <ger@connectedtrips.com>
2013-04-20 13:25:36 +0100

    Got company record saving - yay!

---
+++ eightbitceo/CompanyDataModel.h
@@ -0,0 +1,13 @@
+//
+//  CompanyDataModel.h
+//  eightbitceo
+//
+//  Created by Ger Kelly on 19/04/2013.
+//  Copyright (c) 2013 Getchoo Creations. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CompanyDataModel : NSObject
+
+@end
---
+++ eightbitceo/CompanyDataModel.m
@@ -0,0 +1,13 @@
+//
+//  CompanyDataModel.m
+//  eightbitceo
+//
+//  Created by Ger Kelly on 19/04/2013.
+//  Copyright (c) 2013 Getchoo Creations. All rights reserved.
+//
+
+#import "CompanyDataModel.h"
+
+@implementation CompanyDataModel
+
+@end
